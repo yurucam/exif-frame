65783c9c944ff1feef9fc6b90aa31c48862dbbb7
rhea-so <jeonghyeon.rhea@gmail.com>
2024-04-03 02:03:13 +0900

    feat: Add instagram

--- web/src/pages/frame/page.tsx
+++ web/src/pages/frame/page.tsx
@@ -13,6 +13,9 @@ import insideFrame from './themes/inside.theme';
 import gridLines from './themes/grid-lines.theme';
 import monitor from './themes/monitor.theme';
 import monitorWithoutGridLines from './themes/monitor-without-grid-lines.theme';
+import crop1x1 from './themes/crop-1:1.theme';
+import crop4x5 from './themes/crop-4:5.theme';
+import instagramFrame from './themes/instagram.theme';
 
 const FramePage = () => {
   const frames = [
@@ -25,6 +28,9 @@ const FramePage = () => {
     { name: 'Inside', func: insideFrame },
     { name: 'Monitor', func: monitor },
     { name: 'Monitor Without Grid Lines', func: monitorWithoutGridLines },
+    { name: 'Crop 1:1', func: crop1x1 },
+    { name: 'Crop 4:5', func: crop4x5 },
+    { name: 'Instagram', func: instagramFrame },
   ];
 
   const [photos, setPhotos] = useState<Photo[]>([]);
