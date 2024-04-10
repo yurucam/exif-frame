188fc04dcc73e4924a09d09036505c3ce4be5bf8
rhea-so <jeonghyeon.rhea@gmail.com>
2024-04-10 23:53:58 +0900

    feat: Preview

--- web/src/pages/root/components/remove-one-photo.button.tsx
+++ web/src/pages/root/components/remove-one-photo.button.tsx
@@ -10,15 +10,17 @@ const RemoveOnePhotoButton: React.FC<RemoveOnePhotoButtonProps> = ({ index }) =>
   const { photos, setPhotos } = useStore();
 
   return (
-    <Button
-      className="k-color-brand-red"
-      onClick={() => {
-        const newPhotos = photos.filter((_, i) => i !== index);
-        setPhotos(newPhotos);
-      }}
-    >
-      <Icon ios={<IoTrashOutline className="w-5 h-5" />} />
-    </Button>
+    <div className="w-10">
+      <Button
+        className="k-color-brand-red"
+        onClick={() => {
+          const newPhotos = photos.filter((_, i) => i !== index);
+          setPhotos(newPhotos);
+        }}
+      >
+        <Icon ios={<IoTrashOutline className="w-5 h-5" />} />
+      </Button>
+    </div>
   );
 };
 
