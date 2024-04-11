f2805cb2dee01ebf8772f812f98113657a8980a5
rhea-so <jeonghyeon.rhea@gmail.com>
2024-04-11 22:40:06 +0900

    feat: Support IOS

--- web/src/core/download-one-file.ts
+++ web/src/core/download-one-file.ts
@@ -1,7 +1,17 @@
+import { Capacitor } from '@capacitor/core';
+import { Media } from '@capacitor-community/media';
+import { Buffer } from 'buffer';
 import saveAs from 'file-saver';
 
 const downloadOneFile = async (file: { name: string; buffer: ArrayBuffer; type: 'image/jpeg' | 'image/webp' }): Promise<void> => {
-  saveAs(new Blob([file.buffer], { type: file.type }), file.name.replace(/\.[^/.]+$/, `.${file.type === 'image/jpeg' ? 'jpg' : 'webp'}`));
+  if (Capacitor.isNativePlatform()) {
+    await Media.savePhoto({
+      fileName: file.name.replace(/\.[^/.]+$/, `.${file.type === 'image/jpeg' ? 'jpg' : 'webp'}`),
+      path: `data:${file.type};base64,${Buffer.from(file.buffer).toString('base64')}`,
+    });
+  } else {
+    saveAs(new Blob([file.buffer], { type: file.type }), file.name.replace(/\.[^/.]+$/, `.${file.type === 'image/jpeg' ? 'jpg' : 'webp'}`));
+  }
 };
 
 export default downloadOneFile;
