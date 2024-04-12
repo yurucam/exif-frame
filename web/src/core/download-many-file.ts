666303670758cfc37b8d52e1df2a3f24d1159374
rhea-so <jeonghyeon.rhea@gmail.com>
2024-04-12 20:20:21 +0900

    fix: Add android album

--- web/src/core/download-many-file.ts
+++ web/src/core/download-many-file.ts
@@ -6,11 +6,18 @@ import saveAs from 'file-saver';
 
 const downloadManyFile = async (files: { name: string; buffer: ArrayBuffer; type: 'image/jpeg' | 'image/webp' }[]): Promise<void> => {
   if (Capacitor.isNativePlatform()) {
+    if (!(await Media.getAlbums()).albums.map((album) => album.name).includes('EXIF Frame')) {
+      await Media.createAlbum({ name: 'EXIF Frame' });
+    }
+
     for (const file of files) {
       await Media.savePhoto({
         fileName: file.name.replace(/\.[^/.]+$/, `.${file.type === 'image/jpeg' ? 'jpg' : 'webp'}`),
         path: `data:${file.type};base64,${Buffer.from(file.buffer).toString('base64')}`,
-        albumIdentifier: Capacitor.getPlatform() === 'android' ? (await Media.getAlbums()).albums.pop()?.identifier : undefined,
+        albumIdentifier:
+          Capacitor.getPlatform() === 'android'
+            ? (await Media.getAlbums()).albums.find((album) => album.name === 'EXIF Frame')?.identifier
+            : undefined,
       });
     }
   } else {
--- web/src/core/download-one-file.ts
+++ web/src/core/download-one-file.ts
@@ -6,10 +6,17 @@ import saveAs from 'file-saver';
 const downloadOneFile = async (file: { name: string; buffer: ArrayBuffer; type: 'image/jpeg' | 'image/webp' }): Promise<void> => {
   try {
     if (Capacitor.isNativePlatform()) {
+      if (!(await Media.getAlbums()).albums.map((album) => album.name).includes('EXIF Frame')) {
+        await Media.createAlbum({ name: 'EXIF Frame' });
+      }
+
       await Media.savePhoto({
         fileName: file.name.replace(/\.[^/.]+$/, `.${file.type === 'image/jpeg' ? 'jpg' : 'webp'}`),
         path: `data:${file.type};base64,${Buffer.from(file.buffer).toString('base64')}`,
-        albumIdentifier: Capacitor.getPlatform() === 'android' ? (await Media.getAlbums()).albums.pop()?.identifier : undefined,
+        albumIdentifier:
+          Capacitor.getPlatform() === 'android'
+            ? (await Media.getAlbums()).albums.find((album) => album.name === 'EXIF Frame')?.identifier
+            : undefined,
       });
     } else {
       saveAs(
