c0e4f3b45482f5a32f99c0e51203e43c908111e6
rhea-so <jeonghyeon.rhea@gmail.com>
2024-04-15 20:34:01 +0900

    chore: Rollback album feature

--- web/src/core/file-system/download.ts
+++ web/src/core/file-system/download.ts
@@ -6,17 +6,25 @@ import saveAs from 'file-saver';
  * Download base64 data as a file.
  */
 export default async function download(filename: string, data: string): Promise<void> {
+  // Create an album if it doesn't exist on native platforms. (Android, iOS)
+  if (Capacitor.isNativePlatform()) {
+    const { albums } = await Media.getAlbums();
+    if (!albums.map((album) => album.name).includes('EXIF Frame')) {
+      await Media.createAlbum({ name: 'EXIF Frame' });
+    }
+  }
+
   // Save the file based on the platform.
   switch (Capacitor.getPlatform()) {
     case 'ios':
-      await Media.savePhoto({ fileName: filename, path: data });
+      await Media.savePhoto({
+        fileName: filename,
+        path: data,
+        albumIdentifier: (await Media.getAlbums()).albums.find((album) => album.name === 'EXIF Frame')?.identifier,
+      });
       break;
 
     case 'android':
-      if (!(await Media.getAlbums()).albums.map((album) => album.name).includes('EXIF Frame')) {
-        await Media.createAlbum({ name: 'EXIF Frame' });
-      }
-
       await Media.savePhoto({
         fileName: filename,
         path: data,
