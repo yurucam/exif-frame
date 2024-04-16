926d3e1473d23004630d8ae60d6abd4d10fb15ca
rhea-so <jeonghyeon.rhea@gmail.com>
2024-04-17 03:02:49 +0900

    feat: Add film theme

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -8,6 +8,9 @@ import './locales';
 import './google-analytics';
 import './update-latest-version';
 
+const digital7 = new FontFace('digital-7', 'url(fonts/digital-7.ttf)');
+digital7.load().then((font) => document.fonts.add(font));
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App theme="ios" safeAreas>
