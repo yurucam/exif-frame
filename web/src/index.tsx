b6ee6dae9663996dad8c1f51a877e8e512a5645f
rhea-so <jeonghyeon.rhea@gmail.com>
2024-04-25 20:27:42 +0900

    chore: font enum

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,11 +5,9 @@ import Router from './router';
 
 import './index.css';
 import './locales';
+import './fonts';
 import './google-analytics';
 import './update-latest-version';
-
-const digital7 = new FontFace('digital-7', 'url(fonts/digital-7.ttf)');
-digital7.load().then((font) => document.fonts.add(font));
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
