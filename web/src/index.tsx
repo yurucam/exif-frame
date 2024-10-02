18504e61a979ae78f9e77bfb57c65c7073dbb6ee
rhea-so <jeonghyeon.rhea@gmail.com>
2024-10-02 23:39:47 +0900

    feat(add-shadcn/ui): setup ui framework
    BREAKING CHANGE:

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import './index.css';
+import { Button } from './components/ui/button';
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode></React.StrictMode>
+	<React.StrictMode>
+		<Button>Click me</Button>
+	</React.StrictMode>
 );
