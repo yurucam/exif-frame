f5514b78331d2dc0c2d03f977d0d2f0579fcd302
rhea-so <jeonghyeon.rhea@gmail.com>
2024-10-03 18:15:16 +0900

    feat(i18n): add i18next

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import './App.css';
+
 import {
 	createBrowserRouter,
 	RouterProvider,
---
+++ web/src/i18n/index.tsx
@@ -0,0 +1,26 @@
+import i18n from 'i18next';
+import { initReactI18next } from '../../node_modules/react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import ko from './locales/ko.json';
+import en from './locales/en.json';
+import ja from './locales/ja.json';
+import zh_CN from './locales/zh-CN.json';
+
+const supportLanguages = ['en', 'ja', 'ko', 'zh-CN'];
+
+i18n
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		fallbackLng: 'en',
+		// lng: localStorage.getItem('language') || undefined,
+		supportedLngs: supportLanguages,
+		debug: false,
+		resources: {
+			en: { translation: en },
+			ja: { translation: ja },
+			ko: { translation: ko },
+			'zh-CN': { translation: zh_CN },
+		},
+	});
